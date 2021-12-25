9bae049c924585b67e1c15093e54b0307478f125
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-25 14:43:10 -0500

    Listing 3.5

--- src/Game.kt
+++ src/Game.kt
@@ -1,13 +1,26 @@
 fun main() {
     val name = "Madrigal"
     var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // Aura
+    if (isBlessed && healthPoints > 50 || isImmortal) {
+        println("GREEN")
+    } else {
+        println("NONE")
+    }
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition!")
     } else if (healthPoints >= 90) {
         println(name + " has a few scratches.")
     } else if (healthPoints >= 75) {
-        println(name + " has some minor wounds.")
+        if (isBlessed) {
+            println(name + " has some minor woulds but is healing quite quickly!")
+        } else {
+            println(name + " has some minor wounds.")
+        }
     } else if (healthPoints >= 15) {
         println(name + " looks pretty hurt.")
     } else {
